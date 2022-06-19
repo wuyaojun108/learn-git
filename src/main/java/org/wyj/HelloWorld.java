449bb227cbf0a074f3196f1dc16c7e99cfc68919
wuyaojun <sxslfsfxxx@163.com>
2022-06-19 11:41:41 +0800

    hello world3 amend

--- src/main/java/org/wyj/HelloWorld.java
+++ src/main/java/org/wyj/HelloWorld.java
@@ -5,6 +5,7 @@ import org.wyj.utils.Utils;
 public class HelloWorld {
     public static void main(String[] args) {
         Utils.println("Hello World");
-        Utils.println("hello World2");
+        Utils.println("Hello World2");
+        Utils.println("Hello World3");
     }
 }
