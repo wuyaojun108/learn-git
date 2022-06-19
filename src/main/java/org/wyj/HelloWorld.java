69f86792bfc3346ac27b949b40d836bbd5db340b
wuyaojun <sxslfsfxxx@163.com>
2022-06-19 15:41:18 +0800

    feature_3

--- src/main/java/org/wyj/HelloWorld.java
+++ src/main/java/org/wyj/HelloWorld.java
@@ -6,7 +6,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         Utils.println("Hello World");
         Utils.println("Hello World2");
-        Utils.println("Hello World feature_1");
-        Utils.println("Hello World feature_2");
+        Utils.println("Hello World feature_3");
     }
 }
