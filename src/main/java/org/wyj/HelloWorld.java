f3d2e595f5d460016a30076d08a911ccfe00718d
wuyaojun <sxslfsfxxx@163.com>
2022-06-19 11:40:00 +0800

    hello world2

--- src/main/java/org/wyj/HelloWorld.java
+++ src/main/java/org/wyj/HelloWorld.java
@@ -5,5 +5,6 @@ import org.wyj.utils.Utils;
 public class HelloWorld {
     public static void main(String[] args) {
         Utils.println("Hello World");
+        Utils.println("hello World2");
     }
 }
