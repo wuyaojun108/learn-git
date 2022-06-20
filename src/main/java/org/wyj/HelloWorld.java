d6ccbda2a4034ba2263ea7f089861cc59cf814bb
wuyaojun108 <77907541+wuyaojun108@users.noreply.github.com>
2022-06-20 20:50:04 +0800

    github commit feature_5
    github commit feature_5

--- src/main/java/org/wyj/HelloWorld.java
+++ src/main/java/org/wyj/HelloWorld.java
@@ -10,5 +10,6 @@ public class HelloWorld {
         Utils.println("Hello World feature_2");
         Utils.println("Hello World github");
         Utils.println("Hello World feature_5 idea git");
+        Utils.println("Hello World feature_5 github");
     }
 }
